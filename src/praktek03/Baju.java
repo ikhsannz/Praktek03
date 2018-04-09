51f92143fdb00c90873cff2415f52cfd2b198b82
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-04-09 18:19:29 +0800

    Atrribut dan Method baju

--- src/praktek03/Baju.java
+++ src/praktek03/Baju.java
@@ -2,5 +2,15 @@
 package praktek03;
 
 public class Baju {
+    String merk;
+    String warna;
+    String ukuran;
+    double harga;
     
+    void cetakInfo() {
+        System.out.println("Merk \t:"+merk+"\n"+
+                "Warna \t:"+warna+"\n"+
+                "Ukuran \t:"+ukuran+"\n"+
+                "Harga \t:"+harga);
+    }
 }
