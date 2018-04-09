9e04b180e4b066ec2d19ad9937b0db5dacab4e04
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-04-09 18:27:57 +0800

    menggunakan method set

--- src/praktek03/BajuAksi.java
+++ src/praktek03/BajuAksi.java
@@ -5,10 +5,11 @@ package praktek03;
 public class BajuAksi {
     public static void main(String[] args) {
         Baju a = new Baju();
-        a.merk="cresida";
-        a.warna="biru";
-        a.ukuran="M";
-        a.harga=50000;
+ 
+        a.setMerk("cresida");
+        a.setWarna("biru");
+        a.setUkuran("M");
+        a.setHarga(50000);
         
         a.cetakInfo();
     }
